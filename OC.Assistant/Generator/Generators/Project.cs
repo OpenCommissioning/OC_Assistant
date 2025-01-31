b7ce809b6a8540e91dbd3ee2f50377b29aa883c2
Michael Kasseckert <michael.kasseckert@spiratec.com>
2025-01-31 13:28:33 +0100

    feat: rework of project generator with new mapping feature

--- OC.Assistant/Generator/Generators/Project.cs
+++ OC.Assistant/Generator/Generators/Project.cs
@@ -14,9 +14,6 @@ namespace OC.Assistant.Generator.Generators;
 [SuppressMessage("ReSharper", "SuspiciousTypeConversion.Global")]
 internal static class Project
 {
-    private const string START_REGION = "{region generated code}";
-    private const string END_REGION = "{endregion}";
-    
     /// <summary>
     /// Updates the given plc project.
     /// </summary>
@@ -38,52 +35,25 @@ internal static class Project
             //Iterate elements under main
             foreach (var child in main.Elements())
             {
-                //element is device -> direct instance
-                if (child.Name == "Device")
+                switch (child.Name.LocalName)
                 {
-                    instances.Add(new PouInstanceCall(
-                        name: child.Attribute("Name")?.Value.TcPlcCompatibleString(), 
-                        type: child.Attribute("Type")?.Value, 
-                        comment: child.Element("Comment")?.Value));
+                    case "Device":
+                        instances.Add(new PouInstanceCall(child));
+                        continue;
+                    case "Group":
+                        instances.Add(new PouInstanceCall(child));
+                        CreateGroup(plcProjectItem, child);
+                        continue;
                 }
-
-                //element is group -> create group and instance call
-                if (child.Name != "Group") continue;
-                instances.Add(new PouInstanceCall(
-                    name: child.Attribute("Name")?.Value.TcPlcCompatibleString(), 
-                    type: child.Attribute("Name")?.Value));
-                CreateGroup(plcProjectItem, child);
             }
-
-            //Create main with the given instances
-            CreateMain(mainPrg, instances);
-        });
-    }
-    
-    private static void CreateMain(ITcSmTreeItem? main, List<PouInstanceCall> instances)
-    {
-        Retry.Invoke(() =>
-        {
-            var fbSystem = new PouInstanceCall(name: "fbSystem", type: "FB_System");
             
-            instances.Insert(0, fbSystem);
-            PouStandardization(main, instances);
-            instances.RemoveAt(0);
-
-            //Create Cycle
-            var cycle = CreatePouAction(main, "Cycle");
-
-            //fbSystem and HiL calls on top...
-            var implementationText = XmlFile.HilPrograms?.Aggregate(
-                fbSystem.ImplementationText, 
-                (current, element) => $"{current}\tPRG_{element}();\n");
+            instances.Insert(0, new PouInstanceCall("fbSystem", "FB_System"));
             
-            //...followed by instance calls
-            implementationText = instances.Aggregate(
-                implementationText, 
-                (current, element) => $"{current}{element.ImplementationText}");
+            //HiL calls
+            var additionalCycleText = XmlFile.HilPrograms?.
+                Aggregate("", (current, next) => $"{current}\tPRG_{next}();\r\n");
             
-            Implementation(cycle, implementationText, true);
+            SetPouContent(mainPrg, instances, additionalCycleText);
         });
     }
     
@@ -91,36 +61,28 @@ internal static class Project
     {
         Retry.Invoke(() =>
         {
-            var instances = new List<PouInstanceCall>();
-            
-            var name = group?.Attribute("Name")?.Value;
-
             if (group is null) return;
-            
+            var instances = new List<PouInstanceCall>();
+            var name = group.Attribute("Name")?.Value;
             name = name?.TcPlcCompatibleString();
-            
             var fbName = parentName == "" ? name : $"{parentName}_{name}";
             fbName = fbName?.TcPlcCompatibleString();
-
             var folder = parent?.GetOrCreateChild(name, TREEITEMTYPES.TREEITEMTYPE_PLCFOLDER);
 
             foreach (var child in group.Elements())
             {
-                if (child.Name == "Device")
+                switch (child.Name.LocalName)
                 {
-                    var attr = child.Attribute("Comment");
-                    var comm = attr is null ? "" : attr.Value;
-                    instances.Add(new PouInstanceCall(name: 
-                        child.Attribute("Name")?.Value.TcPlcCompatibleString(), 
-                        type: child.Attribute("Type")?.Value, comment: comm));
+                    case "Device":
+                        instances.Add(new PouInstanceCall(child));
+                        continue;
+                    case "Group":
+                        instances.Add(new PouInstanceCall(
+                            child.Attribute("Name")?.Value, 
+                            $"{fbName}_{child.Attribute("Name")?.Value}"));
+                        CreateGroup(folder, child, fbName);
+                        continue;
                 }
-
-                if (child.Name != "Group") continue;
-                var type = $"{fbName}_{child.Attribute("Name")?.Value}";
-                instances.Add(new PouInstanceCall(
-                    name: child.Attribute("Name")?.Value.TcPlcCompatibleString(), 
-                    type: type));
-                CreateGroup(folder, child, fbName);
             }
 
             CreateFb(folder, fbName, instances);
@@ -131,15 +93,40 @@ internal static class Project
     {
         Retry.Invoke(() =>
         {
-            var fb = parent?.GetOrCreateChild(name, TREEITEMTYPES.TREEITEMTYPE_PLCPOUFB);
+            SetPouContent(parent?.GetOrCreateChild(name, TREEITEMTYPES.TREEITEMTYPE_PLCPOUFB), instances);
+        });
+    }
+    
+    private static void SetPouContent(ITcSmTreeItem? pou, IReadOnlyCollection<PouInstanceCall> instances, string? additionalCycleText = null)
+    {
+        if (pou is null) return;
+        
+        Retry.Invoke(() =>
+        {
+            //Get and clean up implementation and declaration
+            if (pou is not ITcPlcDeclaration decl) return;
+            if (pou is not ITcPlcImplementation impl) return;
+            impl.ImplementationText = Cleanup(impl.ImplementationText);
+            decl.DeclarationText = Cleanup(decl.DeclarationText);
             
-            PouStandardization(fb, instances);
+            //Declaration
+            SetDeclaration(decl, instances.Aggregate("", (current, next) => $"{current}{next.DeclarationText}"));
+            
+            //InitRun action
+            var initRun = CreatePouAction(pou, "InitRun");
+            var initRunText = "\tIF NOT bInitRun THEN RETURN; END_IF\r\n\tbInitRun := FALSE;\r\n";
+            initRunText = instances
+                .Aggregate(initRunText, (current, next) => $"{current}{next.InitRunText}");
+            SetImplementation(initRun, initRunText);
+            
+            //Cycle action
+            var cycle = CreatePouAction(pou, "Cycle");
+            var cycleText = instances
+                .Aggregate("", (current, next) => $"{current}{next.ImplementationText}");
+            SetImplementation(cycle, cycleText + $"{additionalCycleText}");
 
-            //Create cycle
-            var cycle = CreatePouAction(fb, "Cycle");
-
-            //Cycle implementation
-            Implementation(cycle, instances.Select(x => x.ImplementationText), true);
+            //Implementation
+            SetImplementation(impl, "\tInitRun();\r\n\tCycle();\r\n");
         });
     }
     
@@ -154,144 +141,41 @@ internal static class Project
         });
     }
     
-    private static void PouStandardization(ITcSmTreeItem? pou, IReadOnlyCollection<PouInstanceCall> instances)
+    private static void SetImplementation(ITcPlcImplementation? impl, string? text)
     {
-        if (pou is null) return;
+        if (impl is null || string.IsNullOrEmpty(text)) return;
         
         Retry.Invoke(() =>
         {
-            //Get and clean up implementation and declaration
-            var decl = (ITcPlcDeclaration)pou;
-            var impl = (ITcPlcImplementation)pou;
-            impl.ImplementationText = Cleanup(impl.ImplementationText);
-            decl.DeclarationText = Cleanup(decl.DeclarationText);
-
-            //Find and clean up or create actions
-            var initRun = CreatePouAction(pou, "InitRun");
-
-            //InitRun
-            Implementation(initRun, "\tIF NOT bInitRun THEN RETURN; END_IF\n\tbInitRun := FALSE;\n", true);
-
-            //Implementation
-            Implementation(impl, "\tInitRun();\n\tCycle();\n", true);
-
-            //Declaration
-            if (instances.Count > 0)
-            {
-                Declaration(decl, instances.Select(x => x.DeclarationText));
-            }
+            var existingText = impl.ImplementationText;
+            var generatedText = "{region generated code}\r\n";
+            generatedText += text;
+            generatedText += "{endregion}\r\n";
+            generatedText += existingText;
+            impl.ImplementationText = generatedText;
         });
     }
     
-    private static void Implementation(ITcPlcImplementation? impl, IEnumerable<string> lines, bool isOnTop = false)
+    private static void SetDeclaration(ITcPlcDeclaration? decl, string? text)
     {
-        var text = lines.Aggregate("", (current, line) => current + line);
-        Implementation(impl, text, isOnTop);
-    }
-    
-    private static void Implementation(ITcPlcImplementation? impl, string? text, bool isOnTop = false)
-    {
-        if (impl is null) return;
+        if (decl is null || string.IsNullOrEmpty(text)) return;
         
         Retry.Invoke(() =>
         {
-            if (text == "") return;
-
-            var implementationText = "";
-
-            if (isOnTop)
-            {
-                var tmpImpl = impl.ImplementationText;
-                implementationText = START_REGION + "\n";
-                implementationText += text;
-                implementationText += END_REGION + "\n";
-                implementationText += tmpImpl;
-                impl.ImplementationText = implementationText;
-                return;
-            }
-
-            implementationText += "\n" + START_REGION + "\n";
-            implementationText += text;
-            implementationText += END_REGION;
-            impl.ImplementationText = implementationText;
+            var generatedText = "\r\n{region generated code}\r\nVAR_INPUT\r\n";
+            generatedText += $"\tbInitRun : BOOL := TRUE;\r\n{text}";
+            generatedText += "END_VAR\r\n{endregion}";
+            decl.DeclarationText += generatedText;
         });
     }
     
-    private static void Declaration(ITcPlcDeclaration decl, IEnumerable<string> lines)
+    private static string Cleanup(string input)
     {
-        var text = lines.Aggregate("", (current, line) => current + line);
-        Declaration(decl, text);
-    }
-    
-    private static void Declaration(ITcPlcDeclaration decl, string text)
-    {
-        Retry.Invoke(() =>
-        {
-            //Get lines
-            var lines = decl.DeclarationText.Split(["\n"], StringSplitOptions.RemoveEmptyEntries);
-            
-            //Get all text
-            var tempDecl = decl.DeclarationText;
-            
-            //Remove first line from text (which is the program/fb definition e.g. PROGRAM MAIN)
-            if (lines.Any()) tempDecl = tempDecl.Replace(lines[0], "");
-            
-            //Add first line to declaration
-            var declarationText = lines.Any() ? lines[0] : "";
-            
-            //Add generated text to declaration
-            declarationText += "\n" + START_REGION + "\n";
-            declarationText += "VAR_INPUT\n";
-            declarationText += "\tbInitRun : BOOL := TRUE;\n";
-            declarationText += text;
-            declarationText += "END_VAR\n";
-            declarationText += END_REGION;
-             
-            //Add rest of existing text to declaration
-            declarationText += tempDecl;
-
-            decl.DeclarationText = declarationText;
-        });
-    }
-    
-    private static string Cleanup(string text)
-    {
-        var lines = text.Split('\n');
-        var ret = "";
-
-        var startTag = new Regex("^\t?" + START_REGION);
-        var endTag = new Regex("^\t?" + END_REGION);
-        var isGeneratedText = false;
-
-        foreach (var line in lines)
-        {
-            if (!isGeneratedText)
-            {
-                if (startTag.IsMatch(line))
-                {
-                    isGeneratedText = true;
-                    continue;
-                }
-            }
-
-            if (isGeneratedText)
-            {
-                if (endTag.IsMatch(line))
-                {
-                    isGeneratedText = false;
-                    continue;
-                }
-            }
-
-            if (!isGeneratedText)
-                ret += $"{line}\n";
-        }
-
-        ret = ret
-            .Replace("VAR_INPUT\nEND_VAR\n", "")
-            .Replace("VAR_OUTPUT\nEND_VAR\n", "")
-            .Replace("VAR\nEND_VAR\n", "");
-
-        return string.IsNullOrEmpty(ret) ? ret : ret.Remove(ret.Length - 1);
+        var result = Regex
+            .Replace(input, @"\s*\{region generated code\}.*?\{endregion\}\s*", "\r\n", RegexOptions.Singleline)
+            .Replace("VAR_INPUT\r\nEND_VAR\r\n", "")
+            .Replace("VAR_OUTPUT\r\nEND_VAR\r\n", "")
+            .Replace("VAR\r\nEND_VAR\r\n", "");
+        return result == "\r\n" ? "" : result;
     }
 }
--- OC.Assistant/Generator/PouInstanceCall.cs
+++ OC.Assistant/Generator/PouInstanceCall.cs
@@ -1,22 +1,76 @@
-﻿namespace OC.Assistant.Generator;
+﻿using System.Xml.Linq;
+using OC.Assistant.Sdk;
 
-internal class PouInstanceCall(string? name, string? type, string? init = null, string? comment = null)
+namespace OC.Assistant.Generator;
+
+internal class PouInstanceCall
 {
-    private string? Name { get; } = name;
-    private string? Type { get; } = type;
-    private string? Init { get; } = init;
-    private string? Comment { get; } = comment;
+    private readonly string? _name;
+    private readonly string? _type;
+    private readonly string? _comment;
+    private readonly string? _controlAddress;
+    private readonly string? _statusAddress;
+    private readonly string? _assignments;
+    
+
+    public PouInstanceCall(string? name, string? type)
+    {
+        _name = name?.TcPlcCompatibleString();
+        _type = type;
+    }
+    
+    public PouInstanceCall(XElement element)
+    {
+        _name = element.Attribute("Name")?.Value.TcPlcCompatibleString();
+        _type = element.Attribute("Type")?.Value;
+        
+        _comment = element.Element("Comment")?.Value;
+        
+        var address = element.Element("Address");
+        _controlAddress = address?.Attribute("Control")?.Value;
+        _statusAddress = address?.Attribute("Status")?.Value;
+
+        _assignments = element
+            .Elements("Control")
+            .Aggregate("", (current, next) => 
+                current + $"\r\n\t{next.Attribute("Name")?.Value} := GVL_{next.Attribute("Assignment")?.Value},");
+
+        _assignments = element
+            .Elements("Status")
+            .Aggregate(_assignments, (current, next) => 
+                current + $"\r\n\t{next.Attribute("Name")?.Value} => GVL_{next.Attribute("Assignment")?.Value},");
+
+        if (_assignments.Length > 0)
+        {
+            _assignments = _assignments.Remove(_assignments.Length - 1) + "\r\n";
+        }
+    }
 
     public string DeclarationText
     {
         get
         {
-            var declaration = $"\t{Name} : {Type};";
-            if (Init != "") declaration = $"\t{Name} : {Type}({Init});";
-            if (Comment != "") declaration += $" // {Comment}";
-            return declaration + "\n";
+            var declaration = $"\t{_name} : {_type};";
+            if (!string.IsNullOrEmpty(_comment))
+            {
+                declaration += $" // {_comment}";
+            }
+            return declaration + "\r\n";
         }
     }
+    
+    public string ImplementationText => $"{_name}({_assignments});\r\n";
+    
 
-    public string ImplementationText => $"\t{Name}();\n";
+    public string InitRunText
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(_controlAddress) || string.IsNullOrEmpty(_statusAddress))
+            {
+                return "";
+            }
+            return $"\t{_name}.AssignProcessData(aControlData := GVL_{_controlAddress}, aStatusData := GVL_{_statusAddress});\r\n";
+        }
+    }
 }
