ed2a229c923bdf538ae1a85e0ce284adfcd4f70e
Michael Kasseckert <michael.kasseckert@spiratec.com>
2025-03-08 14:59:22 +0100

    feat: version check to get informed about new releases

--- OC.Assistant/Controls/HelpMenu.xaml.cs
+++ OC.Assistant/Controls/HelpMenu.xaml.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace OC.Assistant.Controls;
 
@@ -25,6 +26,19 @@ internal partial class HelpMenu
         }
     }
     
+    private class GitHubLink : Button
+    {
+        public GitHubLink()
+        {
+            const string url = "https://github.com/OpenCommissioning/OC_Assistant";
+            Style = Application.Current.FindResource("LinkButton") as Style;
+            Margin = new Thickness(0, 10, 0, 20);
+            Cursor = Cursors.Hand;
+            Content = url;
+            Click += (_, _) => Process.Start(new ProcessStartInfo {FileName = url, UseShellExecute = true});
+        }
+    }
+    
     private void AppDataOnClick(object sender, RoutedEventArgs e)
     {
         Process.Start("explorer.exe" , Core.AppData.Path);
@@ -35,17 +49,14 @@ internal partial class HelpMenu
         Sdk.Logger.Verbose = ((CheckBox) sender).IsChecked == true;
     }
     
-    private async void AboutOnClick(object sender, RoutedEventArgs e)
+    private void AboutOnClick(object sender, RoutedEventArgs e)
     {
         var content = new StackPanel();
         var stack = content.Children;
         
-        stack.Add(new Label{Content = $"\n{ProductName}\nVersion {Version}\n{CompanyName}\n"});
+        stack.Add(new Label {Content = $"\n{ProductName}\nVersion {Version}\n{CompanyName}"});
 
-        if (await MainWindow.CompareVersion("v1.5.0") is {} latestVersion)
-        {
-            stack.Add(new DownloadLink(latestVersion));
-        }
+        stack.Add(new GitHubLink());
         
         stack.Add(new DependencyInfo(typeof(Sdk.Logger))
         {
@@ -79,7 +90,7 @@ internal partial class HelpMenu
             UrlName = "nuget.org"
         });
         
-        stack.Add(new DependencyInfo("dsian.TcPnScanner.CLI", "latest")
+        stack.Add(new DependencyInfo("dsian.TcPnScanner.CLI", "")
         {
             Url = "https://www.nuget.org/packages/dsian.TcPnScanner.CLI",
             UrlName = "nuget.org"
---
+++ OC.Assistant/Controls/VersionCheck.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+using System.Windows;
+using OC.Assistant.Sdk;
+
+namespace OC.Assistant.Controls;
+
+public partial class VersionCheck
+{
+    public VersionCheck()
+    {
+        InitializeComponent();
+        Loaded += OnLoaded;
+        return;
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(async() =>
+            {
+                try
+                {
+                    const string api = "https://api.github.com/repos/opencommissioning/oc_assistant/releases/latest";
+                    const string url = "https://github.com/opencommissioning/oc_assistant/releases/latest";
+                
+                    var version = Assembly.GetExecutingAssembly().GetName().Version;
+                    Logger.LogInfo(this, $"Current version {version}");
+                    var current = $"v{version?.Major}.{version?.Minor}.{version?.Build}";
+                
+                    using var client = new HttpClient();
+                    client.DefaultRequestHeaders.UserAgent.ParseAdd("request");
+                    
+                    var latest = JsonDocument
+                        .Parse(await client.GetStringAsync(api))
+                        .RootElement
+                        .GetProperty("tag_name")
+                        .GetString();
+                
+                    if (current != latest)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            Visibility = Visibility.Visible;
+                            Content = $"Release {latest} available!";
+                            Click += (_, _) => Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                        });
+                    }
+                }
+                catch
+                {
+                    //Logger.LogWarning(this, $"Unable to fetch latest release from GitHub");
+                }
+            });
+        }
+    }
+}
