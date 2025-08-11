44bd710f3bd2febc26b460106c8b72feaef462c1
Michael Kasseckert <michael.kasseckert@spiratec.com>
2025-08-11 18:28:37 +0200

    fix: move named pipe api to Assistant Core

---
+++ OC.Assistant/Core/ApiMessage.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace OC.Assistant.Core;
+
+internal class ApiMessage(byte[] payload)
+{
+    private byte[] Buffer { get; } = BuildMessage(payload);
+    public const int HEADER_SIZE = 4;
+
+    public new string ToString() => Encoding.UTF8.GetString(Buffer, HEADER_SIZE, Buffer.Length - HEADER_SIZE);
+    
+    public static bool IsHeaderValid(byte[] header)
+    {
+        if (header.Length != HEADER_SIZE)
+        {
+            return false;
+        }
+
+        return header[3] == (byte)(header[2] + (header[0] ^ header[1]));
+    }
+
+    public static int GetExpectedSize(byte[] header)
+    {
+        if (header.Length != HEADER_SIZE)
+        {
+            return 0;
+        }
+        return header[0] << 8 | header[1];
+    }
+
+    private static byte[] BuildHeader(byte[] payload)
+    {
+        var header = new byte[HEADER_SIZE];
+        header[0] = (byte)((payload.Length & 0xFF00) >> 8);
+        header[1] = (byte)(payload.Length & 0x00FF);
+        header[2] = 0x01;
+        header[3] = (byte)(header[2] + (header[0] ^ header[1]));
+        return header;
+    }
+    
+    private static byte[] BuildMessage(byte[] payload)
+    {
+        var header = BuildHeader(payload);
+        var message = new byte[header.Length + payload.Length];
+        Array.Copy(header, 0, message, 0, HEADER_SIZE);
+        Array.Copy(payload, 0, message, HEADER_SIZE, payload.Length);
+        return message;
+    }
+}
---
+++ OC.Assistant/Core/ApiNode.cs
@@ -0,0 +1,92 @@
+﻿using System.IO.Pipes;
+using System.Xml.Linq;
+
+namespace OC.Assistant.Core;
+
+internal class ApiNode : IDisposable
+{
+    private static readonly Lazy<ApiNode> LazyInstance = new(() => new ApiNode());
+    private readonly CancellationTokenSource _cancel = new();
+    private NamedPipeServerStream? _pipeServer;
+    
+    /// <summary>
+    /// Singleton interface for the <see cref="ApiNode"/>.
+    /// </summary>
+    public static ApiNode Interface => LazyInstance.Value;
+    
+    /// <summary>
+    /// Is raised when a config has been received.
+    /// </summary>
+    public event Action<XElement>? ConfigReceived;
+    
+    /// <summary>
+    /// Is raised when the TimeScaling value has been changed.
+    /// </summary>
+    public event Action<double>? TimeScalingReceived;
+
+    private ApiNode()
+    {
+        if (LazyInstance.IsValueCreated) return;
+        Listen();
+    }
+
+    private void Listen()
+    {
+        var cancelToken = _cancel.Token;
+        
+        Task.Run(async () =>
+        {
+            while (!cancelToken.IsCancellationRequested)
+            {
+                _pipeServer = new NamedPipeServerStream(
+                    "OC.Assistant.server", 
+                    PipeDirection.InOut, 
+                    1, 
+                    PipeTransmissionMode.Byte, 
+                    PipeOptions.Asynchronous);
+                
+                await _pipeServer.WaitForConnectionAsync(_cancel.Token);
+                
+                var buffer = new byte[ApiMessage.HEADER_SIZE];
+                
+                if (await _pipeServer.ReadAsync(buffer.AsMemory(0, ApiMessage.HEADER_SIZE), cancelToken) == 
+                    ApiMessage.HEADER_SIZE && ApiMessage.IsHeaderValid(buffer))
+                {
+                    var size = ApiMessage.GetExpectedSize(buffer);
+                    
+                    buffer = new byte[size];
+
+                    if (await _pipeServer.ReadAsync(buffer.AsMemory(0, size), cancelToken) == size)
+                    {
+                        await HandleMessage(new ApiMessage(buffer));
+                    }
+                }
+
+                _pipeServer?.Close();
+            }
+        }, cancelToken);
+    }
+    
+    private async Task HandleMessage(ApiMessage message)
+    {
+        var str = message.ToString();
+        var type = str[..3];
+        var msg = str.Remove(0, 3);
+
+        switch (type)
+        {
+            case "cfg":
+                ConfigReceived?.Invoke(XElement.Load(msg));
+                break;
+            case "tsc":
+                TimeScalingReceived?.Invoke(double.TryParse(msg, out var value) ? value : 1.0);
+                break;
+        }
+    }
+
+    public void Dispose()
+    {
+        _cancel.Cancel();
+        _pipeServer?.Close();
+    }
+}
