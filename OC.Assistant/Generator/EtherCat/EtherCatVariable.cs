2dff561205a557f050cc860372f121158b8fe4b4
Viktor Gaponenko <vikt.gaponenko@gmail.com>
2025-11-23 09:44:08 +0100

    fix: bool part repleace

--- OC.Assistant/Generator/EtherCat/EtherCatVariable.cs
+++ OC.Assistant/Generator/EtherCat/EtherCatVariable.cs
@@ -1,4 +1,5 @@
-﻿using OC.Assistant.Sdk;
+﻿using System.Text.RegularExpressions;
+using OC.Assistant.Sdk;
 
 namespace OC.Assistant.Generator.EtherCat;
 
@@ -10,6 +11,8 @@ namespace OC.Assistant.Generator.EtherCat;
 /// <param name="linkTo">The link information for the 'TcLinkTo' attribute.</param>
 internal class EtherCatVariable(string name, string type, string linkTo)
 {
+    private const string REGEX_BOOL = @"\b(?:bool|bit)(\d+)\b";
+    
     /// <summary>
     /// The name of the variable.
     /// </summary>
@@ -18,10 +21,32 @@ internal class EtherCatVariable(string name, string type, string linkTo)
     /// <summary>
     /// The type of the variable.
     /// </summary>
-    public string Type { get; } = type.Replace(TcType.Bit.Name(), TcType.Bool.Name());
-    
+    public string Type => GetValidType();
+
     /// <summary>
     /// The link information for the 'TcLinkTo' attribute.
     /// </summary>
     public string LinkTo { get; } = linkTo;
+
+    private string GetValidType()
+    {
+        var result = type.Replace(TcType.Bit.Name(), TcType.Bool.Name());
+        
+        result = Regex.Replace(result, REGEX_BOOL, match =>
+        {
+            var number = int.Parse(match.Groups[1].Value);
+
+            return number switch
+            {
+                >= 0 and <= 7 => "BYTE",
+                >= 8 and <= 15 => "WORD",
+                >= 16 and <= 31 => "DWORD",
+                _ => match.Value
+            };
+        },
+            RegexOptions.IgnoreCase      
+        );
+
+        return result;
+    }
 }
